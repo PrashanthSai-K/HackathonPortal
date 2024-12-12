b366145b337375ff6194e83ebc83331b6a97725f
PrashanthSai-K <srisathyasai24680@gmail.com>
2024-12-12 14:54:49 +0530

    Database confi done

--- api/server.js
+++ api/server.js
@@ -1,13 +1,17 @@
-const express = require("express")
-const mysql = require("mysql")
-const cors = require("cors")
-const bodyParser = require("body-parser")
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const sequelize = require("./config/database.js");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+sequelize.sync({force : false}).then(()=>{
+    console.log("connected to db successfully---------->>>");
+})
 
 app.listen(4500, ()=>{
     console.log("listening on 3500");
